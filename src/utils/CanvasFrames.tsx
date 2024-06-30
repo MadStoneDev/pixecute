86c169d2aa78cd1c6837db9501a7a5b73d84f7d1
Richard H. <richard@haddads.net.au>
2024-07-01 01:10:31 +1000

    add frame processes

---
+++ src/utils/CanvasFrames.tsx
@@ -0,0 +1,65 @@
+﻿import { Artwork } from "@/types/canvas";
+
+export const addNewFrame = ({
+  artwork,
+  selectedFrame,
+}: {
+  artwork: Artwork;
+  selectedFrame: number;
+}) => {
+  const framesCount = artwork.frames.length;
+
+  artwork.frames.splice(selectedFrame + 1, 0, artwork.frames[selectedFrame]);
+
+  for (const layer of artwork.layers) {
+    for (let i = 0; i < framesCount; i++) {
+      layer.frames[selectedFrame + 1] = new ImageData(1, 1);
+    }
+  }
+};
+
+export const deleteFrame = ({
+  artwork,
+  selectedFrame,
+}: {
+  artwork: Artwork;
+  selectedFrame: number;
+}) => {
+  artwork.frames.splice(selectedFrame, 1);
+
+  for (const layer of artwork.layers) {
+    for (let i = 0; i < artwork.frames.length; i++) {
+      delete layer.frames[i + 1];
+    }
+  }
+};
+
+export const duplicateFrame = ({
+  artwork,
+  selectedFrame,
+}: {
+  artwork: Artwork;
+  selectedFrame: number;
+}) => {
+  const framesCount = artwork.frames.length;
+
+  artwork.frames.splice(selectedFrame + 1, 0, artwork.frames[selectedFrame]);
+
+  for (const layer of artwork.layers) {
+    for (let i = 0; i < framesCount; i++) {
+      layer.frames[selectedFrame + 1] = layer.frames[selectedFrame];
+    }
+  }
+};
+
+export const changeFrameTiming = ({
+  artwork,
+  selectedFrame,
+  newFrameTiming,
+}: {
+  artwork: Artwork;
+  selectedFrame: number;
+  newFrameTiming: number;
+}) => {
+  artwork.frames[selectedFrame] = newFrameTiming;
+};
