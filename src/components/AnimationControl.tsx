5c96315e97911b7beab8af7b65c9547ea40d1d33
Richard H. <richard@haddads.net.au>
2024-07-07 01:58:53 +1000

    animation optimisations

--- src/components/AnimationControl.tsx
+++ src/components/AnimationControl.tsx
@@ -1,11 +1,12 @@
 ﻿"use client";
 
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { Artwork } from "@/types/canvas";
 import useArtStore from "@/utils/Zustand";
 
 import {
+  IconArrowsRightLeft,
   IconRefresh,
   IconSquareChevronLeftFilled,
   IconSquareChevronRightFilled,
@@ -15,35 +16,19 @@ import {
   IconTriangleFilled,
 } from "@tabler/icons-react";
 
-const ANIMATION_TOOLS = [
-  { name: "Loop", icon: <IconRefresh size={24} /> },
-  {
-    name: "Previous Set",
-    icon: <IconSquareChevronsLeftFilled size={24} />,
-  },
-  {
-    name: "Previous Frame",
-    icon: <IconSquareChevronLeftFilled size={24} />,
-  },
-  {
-    name: "Play",
-    icon: <IconTriangleFilled size={20} className={`rotate-90`} />,
-    toggleIcon: <IconSquareFilled size={18} />,
-  },
-  {
-    name: "Next Frame",
-    icon: <IconSquareChevronRightFilled size={24} />,
-  },
-  {
-    name: "Next Set",
-    icon: <IconSquareChevronsRightFilled size={24} />,
-  },
-];
-
-const AnimationControl = ({ liveArtwork }: { liveArtwork: Artwork }) => {
+const AnimationControl = ({
+  liveArtwork,
+  isLoading,
+  setIsLoading,
+}: {
+  liveArtwork: Artwork;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   // States
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
+  const [isPingPong, setIsPingPong] = useState(false);
 
   // Zustand
   const { selectedFrame, setSelectedFrame } = useArtStore();
@@ -51,6 +36,7 @@ const AnimationControl = ({ liveArtwork }: { liveArtwork: Artwork }) => {
   // Refs
   const frameStartTimeRef = useRef<number>(0);
   const animationFrameRef = useRef<number | null>(null);
+  const isReverseRef = useRef<boolean>(false);
 
   const playAnimation = (time: number) => {
     if (!isPlaying) return;
@@ -66,13 +52,35 @@ const AnimationControl = ({ liveArtwork }: { liveArtwork: Artwork }) => {
       frameStartTimeRef.current = time;
 
       setSelectedFrame((prevFrame) => {
-        const nextFrame = prevFrame + 1;
-        if (!isLooping && nextFrame >= liveArtwork.frames.length) {
-          handlePause();
-          return prevFrame;
-        }
+        const nextFrame = isReverseRef.current ? prevFrame - 1 : prevFrame + 1;
 
-        return (prevFrame + 1) % liveArtwork.frames.length;
+        if (!isLooping) {
+          if (
+            (isReverseRef.current && nextFrame < 0) ||
+            (!isReverseRef.current && nextFrame === liveArtwork.frames.length)
+          ) {
+            handlePause();
+            if (isPingPong) {
+              isReverseRef.current = !isReverseRef.current;
+            }
+            return prevFrame;
+          } else {
+            return nextFrame;
+          }
+        } else {
+          if (isPingPong) {
+            if (
+              (isReverseRef.current && nextFrame === 0) ||
+              (!isReverseRef.current &&
+                nextFrame === liveArtwork.frames.length - 1)
+            ) {
+              isReverseRef.current = !isReverseRef.current;
+            }
+            return nextFrame;
+          } else {
+            return nextFrame % liveArtwork.frames.length;
+          }
+        }
       });
     }
 
@@ -80,7 +88,16 @@ const AnimationControl = ({ liveArtwork }: { liveArtwork: Artwork }) => {
   };
 
   const handlePlay = () => {
-    console.log(isPlaying);
+    if (!isPingPong) {
+      isReverseRef.current = false;
+    } else {
+      if (selectedFrame === 0) {
+        isReverseRef.current = false;
+      } else if (selectedFrame === liveArtwork.frames.length - 1) {
+        isReverseRef.current = true;
+      }
+    }
+
     if (!isPlaying) {
       setIsPlaying(true);
       frameStartTimeRef.current = performance.now();
@@ -107,64 +124,130 @@ const AnimationControl = ({ liveArtwork }: { liveArtwork: Artwork }) => {
   }, [isPlaying]);
 
   return (
+    // KEEP AS BACKUP IF WE WANT TO GO TO VERTICAL LAYOUT INSTEAD
+    // <section
+    //   className={`pointer-events-none flex flex-col items-center gap-2 w-10 rounded-2xl text-neutral-900`}
+    // >
+    // ^^ KEEP AS BACKUP IF WE WANT TO GO TO VERTICAL LAYOUT INSTEAD ^^
+
     <section
-      className={`pointer-events-none flex flex-col items-center gap-2 w-10 rounded-2xl text-neutral-900`}
+      className={`pointer-events-none self-center px-2 flex flex-row justify-center items-center gap-2 w-fit h-10 rounded-2xl text-neutral-900 bg-neutral-100 shadow-2xl shadow-neutral-900 z-50`}
     >
-      {ANIMATION_TOOLS.map((tool, index) => (
-        <button
-          key={index}
-          disabled={
-            (tool.name === "Previous Set" && selectedFrame === 0) ||
-            (tool.name === "Previous Frame" && selectedFrame === 0) ||
-            (tool.name === "Next Frame" &&
-              selectedFrame === liveArtwork.frames.length - 1) ||
-            (tool.name === " Set" &&
-              selectedFrame === liveArtwork.frames.length - 1)
+      {/* Previous Set */}
+      <button
+        disabled={selectedFrame === 0}
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          selectedFrame === 0
+            ? "text-neutral-300"
+            : "hover:bg-primary-600 hover:text-neutral-100"
+        } transition-all duration-300`}
+        onClick={() => {
+          setSelectedFrame(0);
+          handlePause();
+        }}
+      >
+        <IconSquareChevronsLeftFilled size={22} />
+      </button>
+
+      {/* Previous Frame */}
+      <button
+        disabled={selectedFrame === 0}
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          selectedFrame === 0
+            ? "text-neutral-300"
+            : "hover:bg-primary-600 hover:text-neutral-100"
+        }  transition-all duration-300`}
+        onClick={() => {
+          setSelectedFrame(selectedFrame - 1);
+          handlePause();
+        }}
+      >
+        <IconSquareChevronLeftFilled size={22} />
+      </button>
+
+      {/* Play */}
+      <button
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          isPlaying ? "rotate-180" : "rotate-90"
+        } transition-all duration-300`}
+        onClick={() => {
+          if (!isPlaying) {
+            handlePlay();
+          } else {
+            handlePause();
           }
-          className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl transition-all duration-300 ${
-            (tool.name === "Previous Set" && selectedFrame === 0) ||
-            (tool.name === "Previous Frame" && selectedFrame === 0) ||
-            (tool.name === "Next Frame" &&
-              selectedFrame === liveArtwork.frames.length - 1) ||
-            (tool.name === "Next Set" &&
-              selectedFrame === liveArtwork.frames.length - 1)
-              ? "bg-neutral-500 text-neutral-700"
-              : "bg-neutral-100 hover:bg-primary-600 hover:text-neutral-100"
-          } ${
-            tool.name === "Loop" && isLooping
-              ? "bg-primary-600 text-neutral-100"
-              : ""
-          }`}
-          onClick={() => {
-            if (tool.name === "Loop") {
-              setIsLooping(!isLooping);
-              setIsPlaying(false);
-            } else if (tool.name === "Previous Set") {
-              handlePause();
-              setSelectedFrame(0);
-            } else if (tool.name === "Previous Frame") {
-              handlePause();
-              setSelectedFrame(selectedFrame - 1);
-            } else if (tool.name === "Next Frame") {
-              handlePause();
-              setSelectedFrame(selectedFrame + 1);
-            } else if (tool.name === "Next Set") {
-              handlePause();
-              setSelectedFrame(liveArtwork.frames.length - 1);
-            } else if (tool.name === "Play") {
-              if (!isPlaying) {
-                handlePlay();
-                console.log("Playing");
-              } else {
-                handlePause();
-                console.log("Paused");
-              }
-            }
-          }}
-        >
-          {isPlaying && tool.toggleIcon ? tool.toggleIcon : tool.icon}
-        </button>
-      ))}
+        }}
+      >
+        {isPlaying ? (
+          <IconSquareFilled size={18} />
+        ) : (
+          <IconTriangleFilled size={20} />
+        )}
+      </button>
+
+      {/* Next Frame */}
+      <button
+        disabled={selectedFrame === liveArtwork.frames.length - 1}
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          selectedFrame === liveArtwork.frames.length - 1
+            ? "text-neutral-300"
+            : "hover:bg-primary-600 hover:text-neutral-100"
+        } transition-all duration-300`}
+        onClick={() => {
+          setSelectedFrame(selectedFrame + 1);
+          handlePause();
+        }}
+      >
+        <IconSquareChevronRightFilled size={22} />
+      </button>
+
+      {/* Next Set */}
+      <button
+        disabled={selectedFrame === liveArtwork.frames.length - 1}
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          selectedFrame === liveArtwork.frames.length - 1
+            ? "text-neutral-300"
+            : "hover:bg-primary-600 hover:text-neutral-100"
+        } transition-all duration-300`}
+        onClick={() => {
+          setSelectedFrame(liveArtwork.frames.length - 1);
+          handlePause();
+        }}
+      >
+        <IconSquareChevronsRightFilled size={22} />
+      </button>
+
+      {/* Loop */}
+      <button
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          isLooping
+            ? "bg-primary-600/40 border-primary-600 text-primary-600"
+            : ""
+        } transition-all duration-300`}
+        onClick={() => {
+          setIsLooping(!isLooping);
+        }}
+      >
+        <IconRefresh size={24} />
+      </button>
+
+      {/* Ping Pong */}
+      <button
+        className={`pointer-events-auto flex items-center justify-center w-8 aspect-square rounded-2xl border border-transparent ${
+          isPingPong
+            ? "bg-primary-600/40 border-primary-600 text-primary-600"
+            : ""
+        } transition-all duration-300`}
+        onClick={() => {
+          setIsPingPong(!isPingPong);
+
+          if (!isPingPong) {
+            isReverseRef.current = false;
+          }
+        }}
+      >
+        <IconArrowsRightLeft size={22} />
+      </button>
     </section>
   );
 };
